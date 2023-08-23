9a9a3f5de3092de8a234735f8da90339c60d9bf1
Tuan Duong <nhattuan.duong@gmail.com>
2023-08-23 18:12:00 +0700

    *Update

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
   appCode: "kms",
   production: true,
   hmr: false,
-  apiUrl: "http://192.168.0.199:5003",
-  authUrl: "http://192.168.0.199:5003",
-  fakeApiUrl: "http://192.168.0.199:4202",
+  apiUrl: "http://192.168.0.199:5353",
+  authUrl: "http://192.168.0.199:5353",
+  fakeApiUrl: "http://192.168.0.199:4242",
   authHttps: false,
   appConfig: "app-config.json",
 };
